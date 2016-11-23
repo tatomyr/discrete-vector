aa29951324af40b90240b9f9d9f6d372e3a2e09f
tatomyr <andrew.tatomyr@gmail.com>
2016-11-24 00:53:11 +0200

    class Vector extends Array

--- file.js
+++ file.js
@@ -1,25 +1,38 @@
-Array.prototype.setZero = function() {
-  return this.map(() => 0);
-}
-
-Array.prototype.next = function(range = 1) {
-
-  for (let k = 0; k < this.length; ) {
-    if (this[k] >= range) {
-      this[k] = 0;
-      k++;
-    } else {
-      this[k]++;
-      break;
-    }
+class Vector extends Array {
+  constructor(original) {
+    super();
+    for (let i in original) this[i] = 0;
   }
 
-  return this;
+  next(range = 1) {
+    for (let k = 0; k < this.length; k++) {
+      if (this[k] >= range) {
+        this[k] = 0;
+      } else {
+        this[k]++;
+        break;
+      }
+    }
+
+    return this;
+  }
+
+  reset() {
+    for (let i in this) this[i] = 0;
+
+    return this;
+  }
 }
 
-const embodiment = (a, b) => a.reduce((prev, next, i) => prev + next[b[i]], 0);
+// Example #1
+// We have a set of operation duration intervals.
+// Find every possible combination of total duration
+// Assume that there are only two possible duration for each operation - min or max
+console.log();
+console.log('Example #1');
+console.log();
 
-// *****************************************************************************
+const embodiment = (a, b) => a.reduce((prev, item, i) => prev + item[b[i]], 0);
 
 const t = [
   [115, 175],
@@ -37,22 +50,26 @@ const t = [
   [40, 100],
   [20, 30]
 ];
+const J = new Vector(t);
+const T = [];
+for (let n = 0; n < Math.pow(2, t.length); n++) {
+  T.push(embodiment(t, J.next()));
+}
+console.log(T.sort((a, b) => a - b));
 
-let J = t.setZero();
+// Example #2
+// We have TWO sets of weights 'h'
+// Which combinations are possible?
+console.log();
+console.log('Example #2');
+console.log();
 
-console.log(J, embodiment(t, J));
-
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
-console.log(J.next(), embodiment(t, J));
+const h = [7, 1, 4];
+console.log(h);
+console.log('------------------');
+const f = new Vector(h);
+for (let n = 0; n < Math.pow(3, h.length); n++) {
+  const H = h.reduce((prev, item, i)=> prev + item * f[i], 0);
+  console.log(`#${n}`, f, H);
+  f.next(2);
+}
